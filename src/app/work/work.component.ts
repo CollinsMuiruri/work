28b6962bfaee6f8ca995b50ffd74111328c65a1c
Collins Muiruri <wanyekicollins@gmail.com>
2018-03-19 15:31:23 +0300

    Add quote deletion

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -9,11 +9,11 @@ import {Work} from '../work'
 export class WorkComponent implements OnInit {
     works = [
     new Work(1,'Collo Conto','dont be afraid to dream',new Date(2018,3,14) ),
-    new Work(2,'Gregory Isaac','I have to buy cookies for the parrot',new Date(2018,6,9) ),
-    new Work(3, 'Lamar Marie','Diana has her birthday coming up soon',new Date(2018,1,12) ),
-    new Work(4, 'Bob mArley','Pupper likes expensive sancks',new Date(2018,0,18) ),
-    new Work(5, 'Solve math homework','Damn Math',new Date(2018,2,14) ),
-    new Work(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14) ),
+    new Work(2,'Isaac newton','If I have seen further than others, it is by standing upon the shoulders of giants.',new Date(2018,6,9) ),
+    new Work(3, 'Lamar Marie','I can calculate the motion of heavenly bodies, but not the madness of people.',new Date(2018,1,12) ),
+    new Work(4, 'Bob mArley','True riches are not found in possessions but in life...FOREVER',new Date(2018,0,18) ),
+    new Work(5, 'Loe Garric','This most beautiful system of the sun, planets and comets, could only proceed from the counsel and dominion of an intelligent and powerful Being.',new Date(2018,2,14) ),
+    new Work(6, 'Joker smoker','To every action there is always opposed an equal reaction.',new Date(2018,3,14) ),
 
     ]
 
