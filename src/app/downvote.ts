cd02380c01ef9c530e87d20bd1e0059cadb4a0a9
Collins Muiruri <wanyekicollins@gmail.com>
2018-03-19 16:32:14 +0300

    Add downvote function

--- src/app/downvote.ts
+++ src/app/downvote.ts
@@ -1,2 +1,17 @@
-export class Downvote {
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me3',
+  template: `
+    <button (click)="onClickMe3($event)">No! .. Click me!</button>
+    {{clickMessage}}`
+})
+export class ClickMe3Component {
+  clickMessage = 'yes';
+  clicks = 1;
+
+  onClickMe3(event: any) {
+    let evtMsg = event ? ' Event target is ' + event.target.tagName  : '';
+    this.clickMessage = (`Click #${this.clicks--}. ${evtMsg}`);
+  }
 }
