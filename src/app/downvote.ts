4dc24e10ae760d7da6714de81d152063d7dbd894
Collins Muiruri <wanyekicollins@gmail.com>
2019-03-17 19:09:07 +0300

    Update some styling

--- src/app/downvote.ts
+++ src/app/downvote.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-downvote',
   template: `
-    <button (click)="onDownvote($event)">No! .. Click me!</button>
+    <button class= "btn btn-warning"(click)="onDownvote($event)">Downvote</button>
     {{clickMessage}}`
 })
 export class DownvoteComponent {
-  clickMessage = 'yes';
+  clickMessage = '';
   clicks = 1;
 
   onDownvote(event: any) {
-    let evtMsg = event ? ' Event target is ' + event.target.tagName  : '';
-    this.clickMessage = (`Click #${this.clicks--}. ${evtMsg}`);
+    let evtMsg = event ? '' + event.target.tagName  : '';
+    this.clickMessage = (`${this.clicks--}. `);
   }
 }
--- src/app/upvote.ts
+++ src/app/upvote.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upvote',
   template: `
-    <button (click)="onUpvote($event)">No! .. Click me!</button>
+    <button  class = "btn btn-info" (click)="onUpvote($event)">Upvote</button>
     {{clickMessage}}`
 })
 export class UpvoteComponent {
-  clickMessage = 'yes';
+  clickMessage = '';
   clicks = 1;
 
   onUpvote(event: any) {
-    let evtMsg = event ? ' Event target is ' + event.target.tagName  : '';
-    this.clickMessage = (`Click #${this.clicks++}. ${evtMsg}`);
+    let evtMsg = event ? '' + event.target.tagName : '';
+    this.clickMessage = (`${this.clicks++}. `);
   }
 }
